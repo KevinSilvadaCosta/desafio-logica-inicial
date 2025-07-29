87757d6f05d01fdc4c333614eb07348c36a75bd6
KevinSilvadaCosta <kevinsilvadacosta@outlook.com.br>
2025-07-29 16:28:15 -0300

    finalização do projeto

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let nomeDoHeroi = "Zlatan"
-let xpDoHerio = 900
+// Nome e experiência do herói
+let nomeDoHeroi = "Zlatan";
+let xpDoHeroi = 12000;
 
-switch (xpDoHerio){
-    case xpDoHerio < 1000:
-        console.log("Nivel Ferro")
-        break
-}
+// variavel para armazenar o nível
+let nivel = "";
+
+// estrutura de decisão para classificar o nível
+if (xpDoHeroi < 1000){
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000){
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000){
+    nivel = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000){
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000){
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000){
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000){
+    nivel = "Imortal";
+} else if (xpDoHeroi >= 10001 ){
+    nivel = "Radiante";
+} 
+
+// mensagem final
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
+
+
